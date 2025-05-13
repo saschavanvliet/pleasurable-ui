f053afe506df4210cda35d0c20cf5b0db0b2342c
Sascha van Vliet <saschacharlotte@gmail.com>
2025-05-13 16:47:36 +0200

    Header gemaakt #2
    Het openklapmenu werkt nog niet in Javascript, maar het menu zelf wel. Hij is ook responsive!

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// document.querySelector(".menu-toggle").addEventListener("click", function() {
+//     const navLinks = document.querySelector(".nav-links");
+//     navLinks.classList.toggle("open");
+// });
+
+// Werkt nog niet
