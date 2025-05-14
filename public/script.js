7025957b6d97ecc95de01a32cb389efcc4db9a31
Nayomekaia <nayomedoelwijt@gmail.com>
2025-05-14 11:51:24 +0200

    test

--- public/script.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector(".menu-toggle").addEventListener("click", function() {
-//     const navLinks = document.querySelector(".nav-links");
-//     navLinks.classList.toggle("open");
-// });
-
-// Werkt nog niet
