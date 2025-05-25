7088857b06e8f115ac4d96d3d5774de025f92f37
Nayomekaia <nayomedoelwijt@gmail.com>
2025-05-26 01:28:17 +0200

    haalt Json op in project.liquid
    importeert json file en gelinkt met de project.liquid pagina

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
 import express from 'express'
+import express, { response } from 'express'
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
 
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
@@ -20,6 +23,9 @@ const app = express()
 
 // Maak werken met data uit formulieren iets prettiger
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const dataPath = path.join('./project.json');
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
@@ -64,7 +70,24 @@ app.get('/agenda', async function (request, response) {
 // Prikbord (route)
 // GET-route
 app.get('/project', async function (req, res) {
-    res.render('project.liquid', { projects: data.projects, uiState });
+
+  try {
+    // Lees de inhoud van het JSON-bestand waarin projecten worden opgeslagen
+    const file = await readFile(dataPath, 'utf8');
+    
+    // Parse de JSON-inhoud van het bestand naar een JavaScript-object
+    const data = JSON.parse(file);
+    
+    // Render de 'project.liquid' template en geef de projecten door als data
+    res.render('project.liquid', { projects: data.projects });
+  } catch (err) {
+    // Log een foutmelding als er iets misgaat bij het lezen of parsen van het bestand
+    console.error('Er is iets mis gegaan:', err);
+    
+    // Stuur een serverfout terug naar de client
+    res.status(500).send('Server error');
+  }
+});
 
 // foot
 app.get('/foot', async function (request, response) {
