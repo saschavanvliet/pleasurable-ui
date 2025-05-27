4827aeceb361ade842d6085b6686cb1b1e0524f0
Sascha van Vliet <saschacharlotte@gmail.com>
2025-05-27 16:35:24 +0200

    #65 #7 Header gefixt
    - Header gefixt (dynamische image)
    - Positionering div met overlay gefixt (hij versprong steeds in 'inspecteren'.
    - Nayomes feedback verwerkt

--- public/script/script.js
+++ public/script/script.js
@@ -1,16 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-  
-    lazyImages.forEach((img) => {
-      img.addEventListener("load", () => {
-        img.classList.add("loaded");
-      });
-  
-      // Als afbeelding al geladen is (bv. uit de cache)
-      if (img.complete) {
-        img.classList.add("loaded");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
       }
     });
-  
-    console.log(`Aantal lazy images gevonden: ${lazyImages.length}`);
+  });
+
+  document.querySelectorAll('.fade-in-geveltuinafbeelding').forEach(img => {
+    observer.observe(img);
   });
